49c75ebc0224a1515eff61f3e48a6b4e8f468088
Patrick McCarver <patrick@merklebros.com>
2022-06-09 23:43:00 -0400

    Feature: Disable map dragging, housekeeping

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 import mouseGeoJSON from "./data.js"
 
-let map = L.map('map').setView([40.713435, -73.929062], 11);
+const map = L.map('map', { zoomControl: false }).setView([40.713435, -73.929062], 11);
+map.touchZoom.disable();
+map.doubleClickZoom.disable();
+map.scrollWheelZoom.disable();
+map.boxZoom.disable();
+map.keyboard.disable();
+map.setMaxBounds(map.getBounds());
+map.dragging.disable();
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
 
-let mouseIcon = L.icon({
+const mouseIcon = L.icon({
     iconUrl: 'https://img.icons8.com/bubbles/50/undefined/rat-silhuette.png'
 });
 
-let featureOptions = {
-    pointToLayer: createIcon
-}
 function createIcon (feature, latLong) {
     return L.marker(latLong, { icon: mouseIcon })
+}
+
+const featureOptions = {
+    pointToLayer: createIcon
 }
 
 L.geoJSON(mouseGeoJSON, featureOptions).addTo(map)
